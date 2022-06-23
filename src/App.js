c0d647e332db8a8210c4efcb2dac91c0d2525c1d
orlijan <leijanlegaspi@gmail.com>
2022-06-23 03:23:36 -0700

    converting to mysql

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ const App = () => {
   const fetchTask = async (id) => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data= await res.json()
+    console.log(data)
     return data
   }
 
